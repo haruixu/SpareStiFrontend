ca9cd234b24d91d0e4cebe77174ae6332f14d1ba
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-16 08:47:57 +0200

    Changed image for test:e2e in the pipeline file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
-    setupNodeEvents(on, config) {
-      return config;
-    },
+    baseUrl: 'http://localhost:4173/',
+    video: false,
+  },
+  env: {
+    apiUrl: 'http://localhost:8080/',
   },
 });
