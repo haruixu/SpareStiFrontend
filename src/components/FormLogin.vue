c2272486a717602900d8d21a3e9cee892cad9540
Trygve Jørgensen <trygjor@stud.ntnu.no>
2024-04-17 16:13:05 +0200

    feat(login): added simple login component

---
+++ src/components/FormLogin.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const username = ref<string>('')
+const password = ref<string>('')
+
+const emit = defineEmits(['submit'])
+
+const submitForm = () => {
+    emit('submit', { username: username.value, password: password.value })
+}
+</script>
+
+<template>
+    <div class="flex flex-col justify-center gap-5 mx-10">
+        <div class="flex flex-col">
+            <p class="mx-5">Brukernavn</p>
+            <input v-model="username" placeholder="Skriv inn brukernavn" type="text" />
+        </div>
+        <div class="flex flex-col">
+            <p class="mx-5">Passord</p>
+            <input v-model="password" placeholder="Skriv inn passord" type="password" />
+        </div>
+        <button @click="submitForm">Logg inn</button>
+    </div>
+</template>
+
+<style scoped></style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
+import LoginView from '@/components/FormLogin.vue'
 
+function login(payload: { username: string; password: string }) {
+    console.log(payload.username, payload.password)
+}
 </script>
 
 <template>
-    <h1>Logg inn</h1>
+    <div class="grid grid-rows-2 gap-10">
+        <div class="flex flex-col">
+            <div class="flex flex-row gap-5 justify-center">
+                <h3>Logg inn</h3>
+                <h3>Registrer deg</h3>
+            </div>
+            <LoginView @submit="login" />
+        </div>
+        <div class="border-2 border-black flex items-center">
+            <h1>Dette er et bilde</h1>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-
-</style>
