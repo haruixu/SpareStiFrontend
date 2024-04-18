537105f3ad3f91b630dca21dc9c792032ca0bc03
Trygve Jørgensen <trygjor@stud.ntnu.no>
2024-04-18 11:31:46 +0200

    feat(login/register): users can now show / hide password when inputting

--- src/components/FormLogin.vue
+++ src/components/FormLogin.vue
@@ -3,23 +3,38 @@ import { ref } from 'vue'
 
 const username = ref<string>('')
 const password = ref<string>('')
+const showPassword = ref<boolean>(false)
 
 const emit = defineEmits(['submit'])
 
 const submitForm = () => {
     emit('submit', username.value, password.value)
 }
+
+const toggleShowPassword = () => {
+    showPassword.value = !showPassword.value
+}
 </script>
 
 <template>
     <div class="flex flex-col justify-center gap-5 mx-10">
         <div class="flex flex-col">
-            <p class="mx-5">Brukernavn</p>
+            <p class="mx-4">Brukernavn</p>
             <input v-model="username" placeholder="Skriv inn brukernavn" type="text" />
         </div>
         <div class="flex flex-col">
-            <p class="mx-5">Passord</p>
-            <input v-model="password" placeholder="Skriv inn passord" type="text" />
+            <p class="mx-4">Passord</p>
+            <div class="relative">
+                <input
+                    v-model="password"
+                    :type="showPassword ? 'text' : 'password'"
+                    class="w-full"
+                    placeholder="Skriv inn passord"
+                />
+                <button class="absolute right-0 top-1 bg-transparent" @click="toggleShowPassword">
+                    {{ showPassword ? '🔓' : '🔒' }}
+                </button>
+            </div>
         </div>
         <button @click="submitForm">Logg inn</button>
     </div>
