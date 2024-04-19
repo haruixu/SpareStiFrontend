1ff7585bac4ec09968fd1a7a93706a7c98f6ed54
Ina <inamar@stud.ntnu.no>
2024-04-19 15:29:12 +0200

    style(start_page): make the startpage more responsive

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background-container">
       <div>
-          <img src="@/assets/start_page/sti.png" alt="Background" class="w-full">
+          <img src="@/assets/start_page/sti.png" alt="Background" class="w-full bg-cover bg-center overflow-hidden bg-top-center">
 
           <div class="flex flex-col items-center pt-40 absolute top-0 left-0 right-0 z-10">
               <img src="@/assets/start_page/Spare.png" alt="Spare" class="w-1/6 h-auto" />
@@ -9,15 +9,15 @@
               <p class="mt-2 text-2xl font-bold">GJØR SPARING TIL EN LEK!</p>
           </div>
           
-          <div class="absolute top-1/4 lg:right-6 md:right-4 right-0 -translate-y-1/2 flex flex-col items-end space-y-6 lg:pr-28 md:pr-10 pr-2 z-20">
+          <div class="flex flex-col -translate-y-1/2 items-end space-y-6 z-20 absolute sm:top-1/4 sm:left-3/4 sm:w-1/6 top:1/2 w-2/3 right-1/2">
               <button
-                  class="w-full py-3 px-16 border border-[#95E35D] shadow-lg rounded-lg transition-all duration-500 bg-[#95E35D] hover:bg-white text-sm lg:text-base"
+                  class="md:py-3 md:px-16 py-2 px-12 border border-[#95E35D] shadow-lg rounded-lg transition-all duration-500 bg-[#95E35D] hover:bg-white text-xs sm:text-base"
                   @click=""
               >
                   Start her!
               </button>
               <button
-                  class="w-full py-3 px-16 border border-[#95E35D] shadow-lg rounded-lg transition-all duration-500 bg-white hover:bg-[#95E35D] text-sm lg:text-base"
+                  class="md:py-3 md:px-16 py-2 px-12  border border-[#95E35D] shadow-lg rounded-lg transition-all duration-500 bg-white hover:bg-[#95E35D] text-xs sm:text-base"
                   @click=""
               >
                   Logg inn
@@ -31,7 +31,7 @@
 
           <div class="ferie absolute -bottom-[600px] left-10 ml-2 w-50 pl-10">
               <h2>Spar til din neste ferie! 🏖️</h2>
-              <p> Ferie er viktig for å lade batteriene. Spar til din neste ferie og nyt livet!</p>
+              <p> Ferie er viktig for å lade batteriene 🔋. Spar til din neste ferie og nyt livet!</p>
                 <div class="absolute top-50 left-10 right-10 h-24 overflow-hidden bg-[url('@/assets/start_page/skyer.png')] bg-repeat-x bg-center animate-clouds">
                 </div>
                 <div class="absolute top-80 left-10 right-10 h-24 overflow-hidden bg-[url('@/assets/start_page/strand.png')] bg-repeat-x bg-center animate-beach">
