54663d638cdb44a72163a9bfc1b7cd253292c766
Eline Evje <elineev@stud.ntnu.no>
2024-04-22 13:59:11 +0200

    feat: changed logic of registering the user's spending items

--- src/views/ConfigSpendingItemsTotalAmountView.vue
+++ src/views/ConfigSpendingItemsTotalAmountView.vue
@@ -1,42 +1,63 @@
 <template>
     <div class="flex flex-col items-center justify-center min-h-screen px-4 text-center">
-        <h1 class="mb-8 lg:mb-12 text-4xl font-bold">Hvor mye bruker du total per uke på ...</h1>
-        <div class="flex flex-col gap-2 mb-6">
-            <div
-                v-for="(option, index) in options"
-                :key="index"
-                class="flex items-center w-80 p-2 cursor-pointer transition-colors bg-white rounded-lg my-4"
-            >
-                <div class="flex flex-1 justify-start">
-                    <p class="text-2xl font-bold">{{ option.name }}</p>
+        <h1 class="mb-8 lg:mb-12 text-4xl font-bold">Hvor mye bruker du totalt per uke på ...</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
+            <div class="flex flex-col items-center bg-white rounded-lg p-8 shadow-lg w-100">
+                <div
+                    v-for="(option, index) in options.slice(0, 3)"
+                    :key="index"
+                    class="w-full my-4"
+                >
+                    <div class="flex justify-between items-center">
+                        <p class="text-xl font-bold mr-4">{{ option.name }}</p>
+                        <div class="flex items-center w-2/3">
+                            <input
+                                type="text"
+                                v-model="option.amount"
+                                @input="($event) => filterAmount(index, $event)"
+                                class="h-11 px-3 rounded-md text-lg focus:outline-none border-2 w-full"
+                                :class="{
+                                    'border-gray-300': option.amount === '',
+                                    'border-[var(--green)]': option.amount !== ''
+                                }"
+                            />
+                            <p class="text-xl font-bold ml-2">kr</p>
+                        </div>
+                    </div>
                 </div>
-                <div class="flex flex-1 items-center justify-end">
-                    <input
-                        type="text"
-                        v-model="option.amount"
-                        @input="($event) => filterAmount(index, $event)"
-                        class="h-10 px-2 w-2/3 rounded-md text-right text-lg focus:outline-none"
-                        :class="{
-                            'border-2 border-gray-300': option.amount === '',
-                            'border-4 border-[var(--green)]': option.amount !== ''
-                        }"
-                    />
-                    <p class="text-2xl font-bold ml-2">kr</p>
+            </div>
+            <div class="flex flex-col items-center bg-white rounded-lg p-8 shadow-lg w-100">
+                <div
+                    v-for="(option, index) in options.slice(3, 6)"
+                    :key="index"
+                    class="w-full my-4"
+                >
+                    <div class="flex justify-between items-center">
+                        <p class="text-xl font-bold mr-4">{{ option.name }}</p>
+                        <div class="flex items-center w-2/3">
+                            <input
+                                type="text"
+                                v-model="option.amount"
+                                @input="($event) => filterAmount(index + 3, $event)"
+                                class="h-11 px-3 rounded-md text-lg focus:outline-none border-2 w-full"
+                                :class="{
+                                    'border-gray-300': option.amount === '',
+                                    'border-[var(--green)]': option.amount !== ''
+                                }"
+                            />
+                            <p class="text-xl font-bold ml-2">kr</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <ContinueButtonComponent
-            @click="onButtonClick"
-            :disabled="options.some((option) => option.amount === '')"
-            :style="{
-                opacity: options.some((option) => option.amount === '') ? '0.6' : '1',
-                cursor: options.some((option) => option.amount === '') ? 'not-allowed' : 'pointer',
-                filter: options.some((option) => option.amount === '')
-                    ? 'brightness(90%)'
-                    : 'brightness(100%)'
-            }"
-            class="px-10 py-3 text-2xl self-end mt-16 mb-0"
-        ></ContinueButtonComponent>
+        <div class="w-full text-right mb-3 mt-16">
+            <ContinueButtonComponent
+                @click="onButtonClick"
+                :disabled="options.some((option) => option.amount === '')"
+                class="px-10 py-3 text-2xl font-bold mb-4"
+            ></ContinueButtonComponent>
+        </div>
     </div>
 </template>
 
@@ -53,7 +74,10 @@ interface Option {
 const options = ref<Option[]>([
     { name: 'Snus', amount: '' },
     { name: 'Kaffe', amount: '' },
-    { name: 'Kantina', amount: '' }
+    { name: 'Kantina', amount: '' },
+    { name: 'Annet', amount: '' },
+    { name: 'Annet', amount: '' },
+    { name: 'Annet', amount: '' }
 ])
 
 const onButtonClick = () => {
@@ -62,6 +86,14 @@ const onButtonClick = () => {
 
 const filterAmount = (index: number, event: Event) => {
     const input = event.target as HTMLInputElement
-    options.value[index].amount = input.value.replace(/[^\d,]/g, '')
+    let filteredValue = input.value.replace(/[^\d,]/g, '')
+    if (filteredValue.includes(',')) {
+        filteredValue = filteredValue.replace(/,+/g, ',')
+        const firstCommaIndex = filteredValue.indexOf(',')
+        filteredValue =
+            filteredValue.slice(0, firstCommaIndex + 1) +
+            filteredValue.slice(firstCommaIndex + 1).replace(/,/g, '')
+    }
+    options.value[index].amount = filteredValue
 }
 </script>
--- src/views/ConfigSpendingItemsView.vue
+++ src/views/ConfigSpendingItemsView.vue
@@ -1,34 +1,56 @@
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen px-4 text-center">
-        <h1 class="mb-8 lg:mb-24 text-4xl font-bold">Hva bruker du mye penger på?</h1>
-        <div class="flex flex-col gap-14 mb-8">
-            <div v-for="(option, index) in options" :key="index" class="w-80">
-                <input
-                    v-model="option.name"
-                    @input="updateBorder(index)"
-                    class="w-full h-10 p-1 rounded-md text-lg focus:outline-none transition-colors"
-                    :class="
-                        option.name ? 'border-4 border-[var(--green)]' : 'border-2 border-gray-300'
-                    "
-                    type="text"
-                    :placeholder="'Kaffe, snus, kantina, ...'"
-                />
+    <div class="flex flex-col items-center justify-center min-h-screen px-4 sm:px-2 text-center">
+        <h1 class="mb-8 lg:mb-12 text-4xl font-bold">Hva bruker du mye penger på?</h1>
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
+            <div class="flex flex-col items-center bg-white rounded-lg p-8 shadow-lg md:w-96">
+                <div
+                    v-for="buttonText in ['Kaffe', 'Snus', 'Kantina']"
+                    :key="buttonText"
+                    class="w-full my-4"
+                >
+                    <button
+                        :class="[
+                            'w-full h-11 rounded-md text-xl font-bold',
+                            selectedOptions.includes(buttonText)
+                                ? 'border-2 border-[var(--green)]'
+                                : 'border-2 border-gray-300'
+                        ]"
+                        @click="toggleOption(buttonText)"
+                        style="background: transparent"
+                    >
+                        {{ buttonText }}
+                    </button>
+                </div>
+            </div>
+
+            <div class="flex flex-col items-center bg-white rounded-lg p-8 shadow-lg md:w-96">
+                <div v-for="(option, index) in options" :key="`input-${index}`" class="w-full my-4">
+                    <input
+                        v-model="option.name"
+                        :class="[
+                            'w-full h-11 px-3 rounded-md text-xl focus:outline-none transition-colors',
+                            option.name
+                                ? 'border-2 border-[var(--green)]'
+                                : 'border-2 border-gray-300'
+                        ]"
+                        type="text"
+                        placeholder="Annet ..."
+                    />
+                </div>
             </div>
         </div>
-        <ContinueButtonComponent
-            @click="onButtonClick"
-            :disabled="!isValid"
-            :style="{
-                opacity: !isValid ? '0.6' : '1',
-                cursor: !isValid ? 'not-allowed' : 'pointer'
-            }"
-            class="px-10 py-3 text-2xl font-bold self-end mt-14 mb-0"
-        ></ContinueButtonComponent>
+        <div class="w-full text-right mb-3 mt-14">
+            <ContinueButtonComponent
+                @click="onButtonClick"
+                :disabled="!isFormValid"
+                class="px-10 py-3 text-2xl font-bold mb-4 mr-2"
+            ></ContinueButtonComponent>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { computed, ref } from 'vue'
 import ContinueButtonComponent from '@/components/ContinueButtonComponent.vue'
 import router from '@/router'
 
@@ -37,19 +59,27 @@ interface Option {
 }
 
 const options = ref<Option[]>([{ name: '' }, { name: '' }, { name: '' }])
+const selectedOptions = ref<string[]>([])
 
-const updateBorder = (index: number) => {}
+const toggleOption = (option: string) => {
+    const index = selectedOptions.value.indexOf(option)
+    if (index === -1) {
+        selectedOptions.value.push(option)
+    } else {
+        selectedOptions.value.splice(index, 1)
+    }
+}
 
-const isValid = computed(() => {
-    return options.value.every((option) => option.name.trim() !== '')
+const isFormValid = computed(() => {
+    return (
+        selectedOptions.value.length > 0 ||
+        options.value.some((option) => option.name.trim() !== '')
+    )
 })
 
 const onButtonClick = () => {
-    if (isValid.value) {
-        const optionNames = options.value.map((option) => option.name)
+    if (isFormValid.value) {
         router.push('/konfigurasjonSteg4')
-    } else {
-        console.error('Please enter all option names.')
     }
 }
 </script>
