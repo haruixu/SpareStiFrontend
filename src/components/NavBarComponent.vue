02edb302ffc8e9c4785db1567a90c4ed8e3f51d6
Malin Haugland Høli <malin.holi@gmail.com>
2024-04-22 11:41:19 +0200

    refactor: :construction: Add a navbar component to use on different pages

---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="flex space-x-12 justify-center mt-1 text-xl">
+        <img src="../assets/logo.svg" alt="logo" class="w-12 h-12 absolute top-6 left-8" />
+        <router-link to="/">🏠Hjem</router-link>
+        <router-link to="/sparemaal">🎯Sparemål</router-link>
+        <router-link to="/spareutfordringer">💰Spareutfordringer</router-link>
+        <router-link to="/profil">🤭Profil</router-link>
+        <router-link to="/konfigurasjonSteg1">🛠️Konfigurasjon</router-link>
+        <img src="../assets/coins.png" alt="streak" class="w-10 h-10" />
+
+        <button @click="logout" class="py-2 px-6 absolute top-8 right-20 rounded-full focus:outline-none focus:ring focus:ring-black-300"
+        >Logg ut</button>
+    </nav>
+
+    <main>
+        <RouterView />
+    </main>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+
+const logout = () => {
+    console.log('logging out')
+}
+</script>
