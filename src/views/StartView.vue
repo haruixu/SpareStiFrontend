15630438e6252f03577d381614369f08dc89c3ef
Ina <inamar@stud.ntnu.no>
2024-04-22 15:22:20 +0200

    refactor(start_page): update routing to register page

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -22,7 +22,7 @@
         >
             <button
                 class="md:py-3 md:px-0 md:w-3/4 py-2 px-12 w-1/2 border border-[#95E35D] shadow-lg rounded-lg transition-all duration-500 bg-[#95E35D] hover:bg-white text-sm md:text-base"
-                @click="goToLogin"
+                @click="goToRegister"
             >
                 Start her
             </button>
@@ -74,6 +74,10 @@ import { useRouter } from 'vue-router'
 const router = useRouter()
 
 const goToLogin = () => {
-    router.push('/login')
+    router.push('/logginn')
+}
+
+const goToRegister = () => {
+    router.push('/registrer')
 }
 </script>
