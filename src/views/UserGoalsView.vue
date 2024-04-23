d83fbd4d0fd51a9fd64d3f24804fe2b5f0e2692f
Trygve Jørgensen <trygjor@stud.ntnu.no>
2024-04-23 13:15:51 +0200

    feat(goal): goal overview now works with backend

--- src/views/UserGoalsView.vue
+++ src/views/UserGoalsView.vue
@@ -1,26 +1,28 @@
 <script lang="ts" setup>
 import CardGoal from '@/components/CardGoal.vue'
-import PageControl from '@/components/PageControl.vue'
 
 import { useRouter } from 'vue-router'
 import { onMounted, ref } from 'vue'
-import axios from 'axios'
+import authInterceptor from '@/services/authInterceptor'
+import type { Goal } from '@/types/goal'
 
 const router = useRouter()
 
 const currentPage = ref(1)
 const totalPages = ref(1)
 
-onMounted(() => {
-    try {
-        const response = axios.get('http://localhost:8080/users/me/goals', {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
+const goals = ref<Goal[]>([])
+
+onMounted(async () => {
+    await authInterceptor('/users/me/goals')
+        .then((response) => {
+            currentPage.value = response.data.currentPage
+            totalPages.value = response.data.totalPages
+            goals.value = response.data.content
         })
-    } catch (error) {
-        console.error(error)
-    }
+        .catch((error) => {
+            console.error(error)
+        })
 })
 </script>
 
@@ -28,11 +30,8 @@ onMounted(() => {
     <h1 class="font-bold text-center">Dine sparemål</h1>
     <div class="flex flex-col gap-5 items-center">
         <div class="flex flex-col justify-center gap-10 sm:flex-row">
-            <CardGoal />
-            <CardGoal />
-            <CardGoal />
+            <CardGoal v-for="(goal, index) in goals" :key="index" :goal-instance="goal" />
         </div>
-        <PageControl />
         <button @click="router.push('/sparemaal/ny')">Opprett et nytt sparemål</button>
     </div>
 </template>
