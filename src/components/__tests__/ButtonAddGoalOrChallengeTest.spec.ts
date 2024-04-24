cd41edae19fe9fe77b4a06a897d4879f60abedce
Malin Haugland Høli <malin.holi@gmail.com>
2024-04-24 10:58:39 +0200

    test: :white_check_mark: Removed unnessecary tests

--- src/components/__tests__/ButtonAddGoalOrChallengeTest.spec.ts
+++ src/components/__tests__/ButtonAddGoalOrChallengeTest.spec.ts
@@ -12,46 +12,6 @@ describe('ButtonComponent', () => {
         expect(wrapper.exists()).toBe(true)
     })
 
-    it('has the correct classes', () => {
-        const wrapper = mount(ButtonComponent, {
-            props: {
-                buttonText: 'Click me'
-            }
-        })
-        const button = wrapper.find('button')
-        const expectedClasses = [
-            'w-full',
-            'max-w-60',
-            'max-h-12',
-            'bg-green-500',
-            'text-white',
-            'font-bold',
-            'py-2',
-            'rounded-full',
-            'flex',
-            'items-center',
-            'justify-start',
-            'pl-4',
-            'space-x-2',
-            'hover:bg-green-600',
-            'drop-shadow-lg',
-            'focus:outline-none',
-            'focus:ring-2',
-            'focus:ring-green-700',
-            'focus:ring-opacity-50',
-            'shadow-md',
-            'transition',
-            'duration-300',
-            'ease-in-out',
-            'text-xs',
-            'md:text-sm',
-            'lg:text-base'
-        ]
-        expectedClasses.forEach((cls) => {
-            expect(button.classes()).toContain(cls)
-        })
-    })
-
     it('displays the correct button text', () => {
         const wrapper = mount(ButtonComponent, {
             props: {
