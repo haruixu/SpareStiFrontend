0e086018429ba04db03a405e0d3fbddeb57639bc
Eline Evje <elineev@stud.ntnu.no>
2024-04-24 11:35:10 +0200

    chore: ran format

--- src/stores/userConfigStore.ts
+++ src/stores/userConfigStore.ts
@@ -7,35 +7,39 @@ export const useUserConfigStore = defineStore('userConfig', {
         role: 'USER',
         experience: 'VERY_HIGH',
         motivation: 'VERY_HIGH',
-        challengeTypeConfigs: [] as { type: string, specificAmount: number, generalAmount: number }[],
+        challengeTypeConfigs: [] as {
+            type: string
+            specificAmount: number
+            generalAmount: number
+        }[]
     }),
     actions: {
         setExperience(value: string) {
-            this.experience = value;
+            this.experience = value
         },
         setMotivation(value: string) {
-            this.motivation = value;
+            this.motivation = value
         },
         addChallengeTypeConfig(type: string, specificAmount: number, generalAmount: number) {
-            this.challengeTypeConfigs.push({ type, specificAmount, generalAmount });
+            this.challengeTypeConfigs.push({ type, specificAmount, generalAmount })
         },
         async postUserConfig() {
             const payload = {
                 experience: this.experience,
                 motivation: this.motivation,
                 challengeTypeConfigs: Array.from(this.challengeTypeConfigs)
-            };
+            }
 
             try {
-                const response = await authInterceptor.post('/users/me/config/challenge', payload);
-                console.log('Success:', response.data);
+                const response = await authInterceptor.post('/users/me/config/challenge', payload)
+                console.log('Success:', response.data)
             } catch (error: unknown) {
                 if (axios.isAxiosError(error)) {
-                    console.error('Axios error:', error.response?.data || error.message);
+                    console.error('Axios error:', error.response?.data || error.message)
                 } else {
-                    console.error('An unexpected error occurred:', error);
+                    console.error('An unexpected error occurred:', error)
                 }
             }
         }
     }
-});
+})
