168c0bb516d4c72a61f3502f77cb3f636edc5a2e
Ina <inamar@stud.ntnu.no>
2024-04-24 10:52:16 +0200

    refactor(modal): make modal more general

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -3,7 +3,7 @@
         v-if="isModalOpen"
         class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
     >
-        <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+        <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
             <h2 class="font-bold mb-4">{{ title }}</h2>
             <p class="mb-4">{{ message }}</p>
 
@@ -12,19 +12,10 @@
             <div class="flex flex-col justify-center items-center gap-3 mt-3 w-full">
                 <slot name="buttons"></slot>
             </div>
-
-            <div class="flex justify-center mt-4">
-                <button
-                    @click="closeModal"
-                    class="font-bold py-2 px-4 w-1/2 bg-white border-2 border-[#f7da7c] hover:bg-[#f7da7c]"
-                    aria-label="close"
-                >
-                    Lukk
-                </button>
-            </div>
         </div>
     </div>
 </template>
+
 
 <script setup lang="ts">
 
@@ -34,9 +25,4 @@ defineProps({
     isModalOpen: Boolean,
 })
 
-const emits = defineEmits(['update:isModalOpen'])
-
-function closeModal() {
-    emits('update:isModalOpen', false)
-}
 </script>
