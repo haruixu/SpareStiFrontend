8b7dd6686e9a0305fcbdceb08d7c35649d63c57e
Ina <inamar@stud.ntnu.no>
2024-04-25 15:08:27 +0200

    fix: update z-index to 50

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div
         v-if="isModalOpen"
-        class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+        class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
     >
         <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
             <h2 class="font-bold mb-4">{{ title }}</h2>
