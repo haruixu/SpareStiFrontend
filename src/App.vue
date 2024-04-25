039d98f40aad07494654d88f7af910aadcacaee2
Trygve Jørgensen <trygjor@stud.ntnu.no>
2024-04-25 09:29:04 +0200

    refactor(navbar): navbar cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <script setup lang="ts">
 import NavBarComponent from '@/components/NavBarComponent.vue'
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+const showNavBar = computed(() => {
+    return !(
+        route.path == '/' ||
+        route.path == '/registrer' ||
+        route.path == '/logginn' ||
+        route.path == '/forgotPassword'
+    )
+})
 </script>
 
 <template>
-    <NavBarComponent />
+    <NavBarComponent v-if="showNavBar" />
+
+    <main>
+        <RouterView />
+    </main>
 </template>
