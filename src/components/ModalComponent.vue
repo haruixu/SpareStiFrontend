848f92d55b31c35fb63f20ca4e799ed338874bc4
Ina <inamar@stud.ntnu.no>
2024-04-26 13:02:03 +0200

    fix: change max width of modal

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div
         v-if="isModalOpen"
-        class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50"
+        class="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center z-50 md:-mt-5 md:-mb-5"
     >
-        <div class="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full text-center">
+        <div class="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full text-center">
             <h2 class="title font-bold mb-4">{{ title }}</h2>
             <p class="message mb-4">{{ message }}</p>
 
