d771ec431661d347d0dbdc827e95d4bb1514fb9f
lmlnilse <lmlnilse@stud.ntnu.no>
2024-04-26 14:02:06 +0200

    bioAuth test

--- src/views/BioAuthLogin.vue
+++ src/views/BioAuthLogin.vue
@@ -2,11 +2,28 @@
     <corbado-auth :project-id="corbadoProjectID" conditional="yes">
         <input name="username" id="corbado-username" required autocomplete="webauthn"/>
     </corbado-auth>
+    <button @click="testAPIMethod">TestAPI</button>
 </template>
 
 <script setup>
 import '@corbado/webcomponent/pkg/auth_cui.css'
 import '@corbado/webcomponent'
 import {ref} from 'vue'
+import axios from 'axios'
 const corbadoProjectID = ref(import.meta.env.VITE_CORBADO_PROJECT_ID)
+
+
+const testAPIMethod = async () => {
+    await axios
+        .post("https://" + corbadoProjectID.value + ".frontendapi.corbado.io/v1/users/passkey/register/start", {
+            username: "larsmiloni3@gmail.com",
+            fullName: "Lars Mikke Nilsen"
+        })
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+}
 </script>
