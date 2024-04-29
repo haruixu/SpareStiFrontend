49144039b00716ac34462fc359e40acb837d4b19
Ina <inamar@stud.ntnu.no>
2024-04-29 09:26:07 +0200

    refactor: add interactiveSpare modal to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="flex flex-col items-center max-h-[60vh] md:flex-row md:max-h-[80vh] mx-auto">
         <div class="flex flex-col basis-1/3 order-last md:order-first md:basis-1/4 md:pl-10">
-            <InteractiveSpare
-                :speech="speech"
-                :direction="'right'"
-                :pngSize="15"
-                class="opacity-0 h-0 w-0 md:opacity-100 md:h-auto md:w-auto md:mx-auto md:my-20"
-            ></InteractiveSpare>
+            <div class="flex items-center">
+                <img v-if="speech.length > 0" alt="Varsel"
+                     class="scale-x-[-1] md:h-1/6 md:w-1/6 w-1/6 h-1/6 cursor-pointer"
+                     src="@/assets/varsel.png"
+                />
+                <a @click="openInteractiveSpare" class="hover:bg-transparent">
+                    <img alt="Spare"
+                         class="scale-x-[-1] md:h-5/6 md:w-5/6 w-2/3 h-2/3 cursor-pointer"
+                         src="@/assets/spare.png"
+                    />
+                </a>
+            </div>
             <div class="flex flex-row gap-2 items-center mx-auto my-4 md:flex-col md:gap-4 md:m-8">
                 <ButtonAddGoalOrChallenge :buttonText="'Legg til sparemål'" :type="'goal'" />
                 <ButtonAddGoalOrChallenge
@@ -17,6 +23,13 @@
         </div>
         <savings-path :challenges="challenges" :goal="goal"></savings-path>
     </div>
+    <InteractiveSpare
+        :speech="speech"
+        :direction="'right'"
+        :pngSize="15"
+        :is-modal-open="isModalOpen"
+        class="opacity-0 h-0 w-0 md:opacity-100 md:h-auto md:w-auto"
+    ></InteractiveSpare>
 </template>
 
 <script setup lang="ts">
@@ -31,6 +44,7 @@ import SavingsPath from '@/components/SavingsPath.vue'
 
 const goalStore = useGoalStore()
 const challengeStore = useChallengeStore()
+const isModalOpen = ref(false)
 
 const challenges = ref<Challenge[]>([])
 const goals = ref<Goal[]>([])
@@ -43,15 +57,19 @@ onMounted(async () => {
     challenges.value = challengeStore.challenges
     goals.value = goalStore.goals
     goal.value = goals.value[0]
-    console.log('Goals:', goals.value)
 })
 
-// Define your speech array
+// Speech array for InteractiveSpare component
 const speechArray = [
-    'Hei! Jeg er Sparemannen.',
-    'Jeg hjelper deg med å spare penger.',
+    'Hei, jeg er Spare!',
+    'Jeg skal hjelpe deg med å spare penger.',
     'Klikk på meg for å høre mer.'
 ]
 
 const speech = ref(speechArray)
+
+const openInteractiveSpare = () => {
+    isModalOpen.value = true
+    console.log('Modal opened')
+}
 </script>
