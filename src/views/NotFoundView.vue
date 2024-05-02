0432a4921f553ae22c7acf20588ba86ab3e89e0b
Ina <inamar@stud.ntnu.no>
2024-05-02 16:57:18 +0200

    fix: update styling

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup></script>
 
 <template>
-    <div class="flex justify-center items-center mt-16">
+    <div class="flex flex-col justify-center items-center mt-16">
         <h1>404 - Siden finnes ikke</h1>
         <p>Denne siden finnes ikke. Gå tilbake til <RouterLink to="/">hjem</RouterLink>.</p>
     </div>
